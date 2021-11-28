4b4859dc52f6d74cda3690d63ed6f0fd0434acd3
unknown <montasirazad@gmail.com>
2021-11-28 17:02:26 +0600

    title changed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
 
 function App() {
+
+const handleGoogleSignIn =() =>{
+  console.log(new Date())
+}
+
   return (
     <div className="App">
-      <button> Sign in with Google </button>
+      <button onClick={handleGoogleSignIn} style={{marginTop:'150px',backgroundColor:"#7FFFD4"}}> Sign in with Google </button> 
     </div>
   );
 }
